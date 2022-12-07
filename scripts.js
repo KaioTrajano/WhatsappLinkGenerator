02cba14d9429d3fad87597fa9ef0f7e66d814f70
Kaio Trajano <78030357+KaioTrajano@users.noreply.github.com>
2022-12-07 08:58:31 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function generateLink() {
    
     let message = document.form_main.message.value;
     let url = "https://wa.me/";
-    let end_url = `${url}${number}?text=${message}`;
+    let end_url = `${url}55${number}?text=${message}`;
     let urlformatada = end_url.replace(/ /g, "%20")
     document.getElementById('end_url').value = urlformatada;
     botaocopiar.innerHTML = `<i class="fa-regular fa-copy"></i> Copiar`
